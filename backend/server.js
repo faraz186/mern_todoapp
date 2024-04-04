2b1b14459c569a0eeb8f63c4bcebd770963c7545
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-04-05 03:08:40 +0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const todoModel = require('./models/todoSchema');
 const PORT = 8080;  
-const URI='**********************************************************'
+const URI='**********************************************************'  
    
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
