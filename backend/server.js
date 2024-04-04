ae4412f24404932e79b63fe24ceeedd61a263679
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-04-05 03:08:30 +0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ mongoose.connect(URI);
 
 mongoose.connection.on('connected',()=>console.log("database connnect successfully..."));  
 mongoose.connection.on('error',(err)=>console.log(err))
-
+   
 app.post('/api/addtodo',async(req,res)=>{  
     try {
     const {todo} = req.body;
